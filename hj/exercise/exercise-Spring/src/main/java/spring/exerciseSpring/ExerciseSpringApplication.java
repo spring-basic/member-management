57499696910b4a03dd337c39b5c95e2ec16304a7
이현진 <ehj1107@naver.com>
2022-12-20 22:20:39 +0900

    feat: exercise용 프로젝트 추가

---
+++ hj/exercise/exercise-Spring/src/main/java/spring/exerciseSpring/ExerciseSpringApplication.java
@@ -0,0 +1,13 @@
+package spring.exerciseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseSpringApplication.class, args);
+	}
+
+}
---
+++ hj/exercise/exercise-Spring/src/test/java/spring/exerciseSpring/ExerciseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.exerciseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
