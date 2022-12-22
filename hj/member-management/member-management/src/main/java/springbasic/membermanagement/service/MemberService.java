2565fdbed937c505d915eca423327ef2f94b2bd3
이현진 <ehj1107@naver.com>
2022-12-22 15:58:30 +0900

    test: 회원 서비스 테스트

--- hj/member-management/member-management/src/main/java/springbasic/membermanagement/service/MemberService.java
+++ hj/member-management/member-management/src/main/java/springbasic/membermanagement/service/MemberService.java
@@ -8,19 +8,24 @@ import  java.util.List;
 import  java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberReopsitory = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /**
     *회원 가입
      */
     public Long join(Member member){
         validateDuplicateMember(member); //중복 회원 검증
-        memberReopsitory.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member){
-        memberReopsitory.findByName(member.getName()).ifPresent(m -> {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
@@ -29,10 +34,10 @@ public class MemberService {
     *전체 최원 조회
      */
     public List<Member> findMembers(){
-        return memberReopsitory.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
-        return memberReopsitory.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
