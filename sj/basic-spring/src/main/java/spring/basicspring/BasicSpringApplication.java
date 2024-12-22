0dbf725f6329b2c426a644fd7423cef6cd9ce1b9
tpwls9494 <63094026+tpwls9494@users.noreply.github.com>
2024-12-22 12:42:16 +0900

    force the old code to be pulled
    20241222 spring

--- sj/basic-spring/src/main/java/spring/basicspring/BasicSpringApplication.java
+++ sj/basic-spring/src/main/java/spring/basicspring/BasicSpringApplication.java
@@ -1,7 +1,7 @@
 package spring.basicspring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicSpringApplication {
